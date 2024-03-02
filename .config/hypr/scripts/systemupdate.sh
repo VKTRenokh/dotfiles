342c047388d135802601bb007fa56670a33063c5
VKTRenokh <viktorenokh@gmail.com>
2024-03-02 13:22:32 +0200

    update hyprland

--- .config/hypr/scripts/systemupdate.sh
+++ .config/hypr/scripts/systemupdate.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
 if [ ! -f /etc/arch-release ] ; then
-    exit 0
+  exit 0
 fi
 
 aur=$(paru -Qua | wc -l)
 ofc=$(pacman -Qu | wc -l)
 
 upd=$(( ofc + aur ))
-echo "$upd"
 
 if [ $upd -eq 0 ] ; then
-    echo " Packages are up to date"
+  exit 1
 else
-    echo " $ofc Official 󱓾 $aur AUR "
+  echo " $ofc Official 󱓾 $aur AUR "
 fi
 
 if [ "$1" == "up" ] ; then
-    kitty --title systemupdate sh -c 'yay -Syu'
+  kitty --title systemupdate sh -c 'yay -Syu'
 fi
