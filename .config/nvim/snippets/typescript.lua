8f53e98842b676e89f09ebdf2296629ba4bde7ff
VKTRenokh <viktorenokh@gmail.com>
2024-05-28 16:20:02 +0300

    update neovim

---
+++ .config/nvim/snippets/typescript.lua
@@ -0,0 +1,12 @@
+--stylua: ignore start
+return {
+  s('arrow', {
+    c(1, {
+      {
+        t('const '), i(1, "main"), t(' = ('), i(2), t('): '), i(3), t({ ' => {', '' }),
+        t("\t"), i(4), t({'', "}"})
+      }
+    })
+  })
+}
+--stylua: ignore end
---
+++ .config/nvim/snippets/vue.lua
@@ -0,0 +1,11 @@
+--stylua: ignore start
+return {
+  s('vue', {
+    c(1, {
+      {t('<script lang="'), i(1, "ts"), t('">'), i(2), t('</script>')},
+      -- TODO: complete this snippet 
+      {}
+    })
+  })
+}
+--stylua: ignore end
