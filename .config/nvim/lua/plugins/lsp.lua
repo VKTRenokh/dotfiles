6ba275deaca612662367ff16b94fcba039e2fc99
VKTRenokh <viktorenokh@gmail.com>
2024-06-17 23:19:59 +0300

    fix: vuelsp wasn't working

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -98,6 +98,15 @@ return {
             },
           },
         },
+        volar = {
+          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+          init_options = {
+
+            vue = {
+              hybridMode = false,
+            },
+          },
+        },
         tsserver = {
           enabled = false,
         },
