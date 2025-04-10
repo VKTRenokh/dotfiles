3727915b971de2dae994ba97f5586b07222a0535
VKTRenokh <viktorenokh@gmail.com>
2025-04-10 11:14:48 +0300

    feat: remove fzf-lua in favour of snacks.picker

--- .config/nvim/lua/plugins/finders.lua
+++ .config/nvim/lua/plugins/finders.lua
@@ -1,30 +1,6 @@
 Constants = require("config.constants")
 
 return {
-  -- {{{ fzf-lua
-  {
-    "ibhagwan/fzf-lua",
-    cmd = "FzFLua",
-    keys = {
-        -- stylua: ignore start
-      { ";f", function() require("fzf-lua").files() end },
-      { ";r", function () require('fzf-lua').live_grep() end },
-      { ";e", function () require('fzf-lua.providers.diagnostic').diagnostics() end },
-      { ";s", function () require('fzf-lua.providers.lsp').document_symbols() end },
-      { "gr", function () require('fzf-lua.providers.lsp').references() end },
-      { "gd", function () require('fzf-lua.providers.lsp').references() end },
-      -- stylua: ignore end
-    },
-    dependencies = { "echasnovski/mini.icons" },
-    opts = {
-      winopts = {
-        fullscreen = false,
-      },
-      preview = {
-        title_pos = "left",
-      },
-    },
-  }, -- }}}
   -- {{{ oil.nvim
   {
     "stevearc/oil.nvim",
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -395,6 +395,30 @@ return {
     priority = 1000,
     enabled = true,
     lazy = false,
+    keys = {
+      -- stylua: ignore start
+      { ";f", function() Snacks.picker.files() end },
+      { ";r", function () Snacks.picker.grep() end },
+      { ";e", function () require('fzf-lua.providers.diagnostic').diagnostics() end },
+      { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+      { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+      { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+      { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+      { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+      { ";s", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+      { ";S", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+
+      { ";gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+      { ";g", function() Snacks.picker.git_log() end, desc = "Git Log" },
+      { ";gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+      { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
+      { ";gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
+      { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+
+     { ";t", function() Snacks.picker.help() end, desc = "Help Pages" },
+
+      -- stylua: ignore end
+    },
     opts = {
       bigfile = { enabled = true },
       notifier = { enabled = true },
