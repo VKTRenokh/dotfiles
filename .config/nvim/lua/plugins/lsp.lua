48b91e68fcc850fb0eb28efb761d9a9c6cf378e5
VKTRenokh <viktorenokh@gmail.com>
2025-08-18 21:30:34 +0300

    fix: vue ls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,48 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-        ensure_installed = { "lua_ls", "vtsls", "vue_ls", "cssls", "eslint", "html", "jsonls"  },
-    },
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
+  "mason-org/mason-lspconfig.nvim",
+  lazy = false,
+  opts = {
+    ensure_installed = { "lua_ls", "vtsls", "vue_ls", "cssls", "eslint", "html", "jsonls" },
+    automatic_enable = {
+      exclude = { "vtsls", "vue_ls" }
+    }
+  },
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    {
+      "neovim/nvim-lspconfig",
+      config = function()
+        local vue_language_server_path = vim.fn.stdpath('data') ..
+            "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+
+        local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+        local vue_plugin = {
+          name = '@vue/typescript-plugin',
+          location = vue_language_server_path,
+          languages = { 'vue' },
+          configNamespace = 'typescript',
+        }
+        local vtsls_config = {
+          settings = {
+            vtsls = {
+              tsserver = {
+                globalPlugins = {
+                  vue_plugin,
+                },
+              },
+            },
+          },
+          filetypes = tsserver_filetypes,
+        }
+
+        vim.lsp.config('vtsls', vtsls_config)
+
+        vim.lsp.enable('vue_ls')
+        vim.lsp.enable('vtsls')
+      end
+    }
+  },
+  setup = function(_, opts)
+    require("mason-lspconfig").setup(opts)
+  end,
 }
