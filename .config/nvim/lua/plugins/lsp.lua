77e7840c05ecb579e3f1f7521a6c40df88114fbe
VKTRenokh <viktorenokh@gmail.com>
2025-08-20 10:06:12 +0300

    feat: add fidget.nvim & change lsp diagnostics configuration

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,8 +4,8 @@ return {
   opts = {
     ensure_installed = { "lua_ls", "vtsls", "vue_ls", "cssls", "eslint", "html", "jsonls" },
     automatic_enable = {
-      exclude = { "vtsls", "vue_ls" }
-    }
+      exclude = { "vtsls", "vue_ls" },
+    },
   },
   dependencies = {
     { "mason-org/mason.nvim", opts = {} },
@@ -13,16 +13,17 @@ return {
     {
       "neovim/nvim-lspconfig",
       config = function()
-        local vue_language_server_path = vim.fn.stdpath('data') ..
-            "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+        local vue_language_server_path = vim.fn.stdpath("data")
+          .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
 
-        local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+        local tsserver_filetypes =
+          { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
 
         local vue_plugin = {
-          name = '@vue/typescript-plugin',
+          name = "@vue/typescript-plugin",
           location = vue_language_server_path,
-          languages = { 'vue' },
-          configNamespace = 'typescript',
+          languages = { "vue" },
+          configNamespace = "typescript",
         }
 
         local vtsls_config = {
@@ -38,11 +39,26 @@ return {
           filetypes = tsserver_filetypes,
         }
 
-        vim.lsp.config('vtsls', vtsls_config)
+        vim.lsp.config("vtsls", vtsls_config)
 
         vim.lsp.enable({ "vue_ls", "vtsls" })
-      end
-    }
+
+        vim.diagnostic.config({
+          update_in_insert = false,
+          virtual_lines = {
+            current_line = true,
+          },
+          signs = {
+            text = {
+              [vim.diagnostic.severity.ERROR] = "",
+              [vim.diagnostic.severity.WARN] = "",
+              [vim.diagnostic.severity.HINT] = "",
+              [vim.diagnostic.severity.INFO] = "",
+            },
+          },
+        })
+      end,
+    },
   },
   setup = function(_, opts)
     require("mason-lspconfig").setup(opts)
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -63,166 +63,6 @@ return {
   },
 
   -- ----------------------------------------------------------------------- }}}
-  -- {{{ lualine.nvim
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "LazyFile",
-    enabled = false,
-    opts = function(_, opts)
-      local hide_in_width = function()
-        return vim.fn.winwidth(0) > 80
-      end
-
-      local function fg(name)
-        return function()
-          ---@type {foreground?:number}?
-          local hl = vim.api.nvim_get_hl_by_name(name, true)
-          return hl and hl.foreground and { fg = string.format("#%06x", hl.foreground) }
-        end
-      end
-
-      local diagnostics = {
-        "diagnostics",
-        sources = { "nvim_diagnostic" },
-        sections = { "error", "warn" },
-        symbols = {
-          error = Icons.diagnostics.Error,
-          warn = Icons.diagnostics.Warning,
-          info = Icons.diagnostics.Information,
-          hint = Icons.diagnostics.Hint,
-        },
-        colored = true,
-        update_in_insert = false,
-        always_visible = true,
-      }
-
-      local diff = {
-        "diff",
-        colored = true,
-        symbols = { added = Icons.git.Add, modified = Icons.git.Mod, removed = Icons.git.Remove },
-        cond = hide_in_width,
-      }
-
-      local mode = {
-        "mode",
-        fmt = function(str)
-          return string.lower(str)
-        end,
-      }
-
-      local filetype = {
-        "filetype",
-        icons_enabled = true,
-        icon = nil,
-        colored = true,
-      }
-
-      local branch = {
-        "branch",
-        icons_enabled = true,
-        icon = Icons.git.Branch,
-      }
-
-      local location = {
-        "location",
-        padding = 0,
-      }
-
-      local progress = function()
-        local current_line = vim.fn.line(".")
-        local total_lines = vim.fn.line("$")
-        local chars =
-          { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-        local line_ratio = current_line / total_lines
-        local index = math.ceil(line_ratio * #chars)
-        return chars[index]
-      end
-
-      local spaces = function()
-        return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
-      end
-
-      opts.options = {
-        icons_enabled = true,
-        theme = "auto",
-        component_separators = { left = Icons.ui.SlashLeft, right = Icons.ui.SlashRight },
-        section_separators = {
-          left = Icons.ui.BigCircleSeparatorLeft,
-          right = Icons.ui.BigCircleSeparatorRight,
-        },
-        disable_filetypes = { "dashboard", "lazy", "NvimTree", "Outline", "alpha" },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = true,
-        refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
-        },
-      }
-
-      opts.sections = {
-        lualine_a = { mode },
-        lualine_b = { branch },
-        lualine_c = {
-          {
-            "filename",
-            file_status = true,
-            path = 0,
-          },
-          { "aerial" },
-        },
-        lualine_x = {
-          {
-            function()
-              return require("noice").api.status.command.get()
-            end,
-            cond = function()
-              return package.loaded["noice"] and require("noice").api.status.command.has()
-            end,
-            color = fg("Statement"),
-          },
-          {
-            function()
-              return require("noice").api.status.mode.get()
-            end,
-            cond = function()
-              return package.loaded["noice"] and require("noice").api.status.mode.has()
-            end,
-            color = fg("Constant"),
-          },
-          diagnostics,
-          diff,
-          spaces,
-          "encoding",
-          filetype,
-        },
-        lualine_y = { progress },
-        lualine_z = { location },
-      }
-
-      opts.inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {
-          {
-            "filename",
-            file_status = true,
-            path = 1,
-          },
-        },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-      }
-
-      opts.tabline = {}
-      opts.winbar = {}
-      opts.inactive_winbar = {}
-      opts.extensions = { "fugitive" }
-    end,
-  },
-  -- ----------------------------------------------------------------------- }}}
   -- {{{ noice.nvim
   {
     "folke/noice.nvim",
@@ -357,8 +197,8 @@ return {
       { ";e",         function() require('fzf-lua.providers.diagnostic').diagnostics() end },
       { "gd",         function() Snacks.picker.lsp_definitions() end,                      desc = "Goto Definition" },
       { "gD",         function() Snacks.picker.lsp_declarations() end,                     desc = "Goto Declaration" },
-      { "gr",         function() Snacks.picker.lsp_references() end,                       nowait = true,                  desc = "References" },
-      { "gI",         function() Snacks.picker.lsp_implementations() end,                  desc = "Goto Implementation" },
+      { "grr",         function() Snacks.picker.lsp_references() end,                       nowait = true,                  desc = "References" },
+      { "gri",         function() Snacks.picker.lsp_implementations() end,                  desc = "Goto Implementation" },
       { "gy",         function() Snacks.picker.lsp_type_definitions() end,                 desc = "Goto T[y]pe Definition" },
       { ";s",         function() Snacks.picker.lsp_symbols() end,                          desc = "LSP Symbols" },
       { ";S",         function() Snacks.picker.lsp_workspace_symbols() end,                desc = "LSP Workspace Symbols" },
@@ -493,4 +333,14 @@ return {
     opts = {},
   },
   -- }}}
+  {
+    "j-hui/fidget.nvim",
+    event = "LazyFile",
+    opts = {
+      -- options
+      notification = {
+        override_vim_notify = true,
+      },
+    },
+  },
 }
