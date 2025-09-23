dca75c242a60d43fa75f7767ebd4cef0d09861de
VKTRenokh <viktorenokh@gmail.com>
2025-09-23 11:36:41 +0300

    feat: remove tabstops

--- .config/nvim/queries/fennel/tabstop.scm
+++
@@ -1,9 +0,0 @@
-(fn_form
-  name: (symbol) @tabstop
-  args: (sequence_arguments) @tabstop
-  item: (list) @tabstop)
-
-(local_form 
-  (binding_pair
-    lhs: (symbol_binding) @tabstop
-    rhs: _ @tabstop))
