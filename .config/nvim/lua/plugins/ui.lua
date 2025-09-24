9c8385585be37a5fdc9042e1f0a445468f34049c
VKTRenokh <viktorenokh@gmail.com>
2025-09-24 10:21:31 +0300

    feat: return mode = tabs

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -314,6 +314,7 @@ return {
     },
     opts = {
       options = {
+        mode = 'tabs',
         separator_style = "thin",
         show_buffer_close_icons = false,
         show_close_icon = false,
